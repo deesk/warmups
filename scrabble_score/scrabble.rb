09802fafb61bd54996d5dbfe65e1ea2ba70ca494
Sandesh Karki <de.sandesh@gmail.com>
2017-03-11 23:06:43 +1100

    without ext

---
+++ scrabble_score/scrabble.rb
@@ -0,0 +1,36 @@
+
+require 'pry'
+
+class Scrabble
+
+  @@letters= {
+      1 => 'AEIOULNRST',
+      2 => 'DG',
+      3 => 'BCMP',
+      4 => 'FHVWY',
+      5 => 'K',
+      8 => 'JX',
+      10 => 'QZ'
+    }
+
+  def self.score(str)
+
+    arr = []
+
+    @@letters.each do |key, value|
+      lower = value.downcase
+
+      str.each_char do |c|
+        if (lower.include? (c))
+           arr << key
+        end
+      end
+    end
+
+    user_score = arr.reduce(:+)
+    p user_score
+
+  end
+
+end
+binding.pry
